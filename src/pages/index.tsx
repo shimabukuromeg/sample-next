e04a26853081db7710426adf3da352e384dba2ae
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2021-01-30 14:29:13 +0900

    [update] idとmessageのリストを受け取ったらリスト表示されるコンポーネントを作成した

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,16 @@
-export default function Home() {
+import { NextPage } from 'next';
+import Head from 'next/head';
+import { List } from '../components/Todo/List';
+
+const Home: NextPage = () => {
   return (
       <div>
-        <p>Hello, World.</p>
+          <Head>
+              <title>Sample Next with TypeScript &amp; RTK</title>
+          </Head>
+          <List />
       </div>
   )
 }
+
+export default Home;
