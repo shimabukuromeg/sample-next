a1649459183d73c2b0056e72b533ae7989f2855b
Shimabukuro Megumi <shimabukuromeg@gmail.com>
2021-01-30 15:14:42 +0900

    [update] entityAdapterを利用するようにした

--- src/components/Todo/List.tsx
+++ src/components/Todo/List.tsx
@@ -1,17 +1,11 @@
 import { FC } from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../../reducers';
-import { TodoType } from '../../slices/todoListSlice';
+import { TodoType, selectAll } from '../../slices/todoListSlice';
 import { Todo } from './Todo';
 
-const todoList: TodoType[] = [
-    { id: 1, message: 'todo!' },
-    { id: 2, message: 'complete todo!' },
-    { id: 3, message: 'complete todo!' },
-];
-
 export const List: FC = () => {
-    const todoList = useSelector((state: RootState) => state.todoList)
+    const todoList = useSelector((state: RootState) => selectAll(state.todoList))
     return (
         <ul>
             { todoList.map((todo: TodoType) => <Todo key={todo.id} todo={todo} />) }
